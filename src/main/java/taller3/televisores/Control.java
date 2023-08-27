ecfc906327dd11dc4d1f2117719ad5166defae35
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-08-26 22:11:12 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,5 +1,42 @@
 package taller3.televisores;
 
 public class Control {
+	private TV tv;
+	
+	public Control (TV tv) {
+		this.tv=tv;
+	}
+	public void turnOn() {
+		tv.turnOn();
+	}
+	
+	public void turnOff() {
+		tv.turnOff();
+	}
+	
+	public void canalUp() {
+		tv.canalUp();
+	}
+	
+	public void canlaDown() {
+		tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+	
+	public void setVolumen(int volumen) {
+		tv.setVolumen(volumen);
+	}
+	
 
 }
