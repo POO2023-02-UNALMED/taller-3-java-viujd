52ac58856ed45f48aec8d3a02e83e80ab9c87010
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-08-27 12:37:43 -0500

    d

---
+++ src/test/java/test/Control.java
@@ -0,0 +1,60 @@
+package test;
+
+public class Control {
+	private TV tv;
+	
+	//public Control (TV tv) {
+		//this.tv=tv;
+	//}
+	
+	public void turnOn() {
+		tv.turnOn();
+	}
+	
+	public void turnOff() {
+		tv.turnOff();
+	}
+	
+	public void canalUp() {
+		tv.canalUp();
+	}
+	
+	public void canlaDown() {
+		tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		if (canal>=0 && canal<=120) {
+		tv.setCanal(canal);
+		}
+	}
+	
+	public void setVolumen(int volumen) {
+		if (volumen>=0 && volumen<=7) {
+		tv.setVolumen(volumen);
+		}
+	}
+	
+	public void enlazar(TV tv) {
+		this.tv=tv;
+		tv.setContro(this);
+	}
+	
+	public TV getTv() {
+		return tv;
+	}
+	
+	public void setTv(TV tv) {
+		this.tv=tv;
+	}
+	
+
+}
---
+++ src/test/java/test/Marca.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Marca {
+	private String nombre;
+
+	public Marca (String nombre) {
+		this.nombre=nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+
+}
