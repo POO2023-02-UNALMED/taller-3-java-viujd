83e45b9f3a9af506425c6b162d2361a983e999f7
Juan David Robayo <judavidr@unal.edu.co>
2023-08-27 08:51:46 -0500

    f

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -1,105 +1,107 @@
 package taller3.televisores;
 
 public class TV {
-	private Marca marca;
-	private int canal = 1;
-	private int precio = 500;
-	private boolean estado;
-	private int volumen = 1;
-	private Control control;
-	static int numTV;
-	
-	public TV(Marca marca,boolean estado) {
+		private Marca marca;
+		private int canal;
+		private int precio;
+		private boolean estado;
+		private int volumen;
+		private Control control;
+		private static int numTV=0; 
+		 
+	public TV (Marca marca, boolean estado) {
 		this.marca = marca;
-		this.estado = estado;
-		numTV++;
+	    this.estado = estado;
+	    this.canal=1;
+	    this.volumen=1;
+	    this.precio=500;
+	    numTV++;
 	}
-	
-	public void setMarca(Marca marca) {
-		this.marca = marca;
-	}
-	
+
 	public Marca getMarca() {
-		return marca;
+	    return marca;
 	}
-	
-	public void setControl(Control control) {
-		this.control = control;
+
+	public void setMarca(Marca marca) {
+	    this.marca = marca;
+	}
+
+	public int getCanal() {
+	    return canal;
+	}
+
+	public void setCanal(int canal) {
+		if (estado==true && canal>= 1 && canal<=120) {
+	    this.canal = canal;
+		}
+	}
+
+	public int getPrecio() {
+	    return precio;
+	}
+
+	public void setPrecio(int precio) {
+	    this.precio = precio;
+	}
+
+	public int getVolumen() {
+	    return volumen;
+	}
+
+	public void setVolumen(int volumen) {
+		if (estado==true && volumen>= 1 && volumen<=7) {
+		    this.volumen = volumen;
+			}
 	}
 	
 	public Control getControl() {
 		return control;
 	}
 	
-	public void setPrecio(int precio) {
-		this.precio = precio;
-	}
-	
-	public int getPrecio() {
-		return precio;
-	}
-	
-	public void setVolumen(int volumen) {
-		if(volumen >= 0 && volumen <= 7 && estado == true) {
-			this.volumen = volumen;
-		}	
-	}
-	
-	public int getVolumen() {
-		return volumen;
-	}
-	
-	public void setCanal(int canal) {
-		if(canal >= 1 && canal <= 120 && estado == true) {
-			this.canal = canal;
-		}
-		
-	}
-	
-	public int getCanal() {
-		return canal;
+	public void setContro(Control control) {
+		this.control=control;
 	}
 	
 	public static int getNumTV() {
 		return numTV;
 	}
+	public static void setNumTV(int nume) {
+		numTV=nume;}
 	
-	public static void setNumTV(int num) {
-		numTV = num;
-	}
 	public void turnOn() {
-		estado = true;
+		this.estado=true;
 	}
 	
 	public void turnOff() {
-		estado = false;
+		this.estado=false;
 	}
 	
-	public boolean getEstado() {
+	public boolean gestEstado() {
 		return estado;
 	}
 	
 	public void canalUp() {
-		if(canal >= 1 && canal < 120 && estado == true) {
-			canal++;
+		if (estado==true && canal<120) {
+		canal++;
 		}
 	}
 	
 	public void canalDown() {
-		if(canal > 1 && canal <= 120 && estado == true) {
-			canal--;
+		if (estado==true && canal>1) {
+		canal--;
 		}
 	}
 	
 	public void volumenUp() {
-		if(volumen >= 0 && volumen < 7 && estado == true) {
-			volumen++;
+		if (estado==true && volumen<7) {
+		volumen++;
 		}
 	}
 	
 	public void volumenDown() {
-		if(volumen > 0 && volumen <= 7 && estado == true) {
-			volumen--;
+		if (estado==true && volumen>1) {
+		volumen--;
 		}
 	}
+
 }
